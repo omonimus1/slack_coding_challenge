5875fee73456b08c25edd5858d571c5c7a736a3e
Davide Pollicino <davidepollicino2015@gmail.com>
2021-06-17 00:09:22 +0100

    Complete daily challenge bot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # slack_coding_challenge
+
+https://howchoo.com/python/python-send-slack-messages-slackclient
+sudo pip3 install xlrd
+pip install slackclient
+pip3 install pandas
+pip3 install openpyxl
---
+++ get_challenge.py
@@ -0,0 +1,52 @@
+
+import slack
+import random 
+import logging
+import traceback
+import sys
+
+
+# Configure Basic Logging
+logging.basicConfig(filename='error.log',
+                            filemode='a',
+                            format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.ERROR)
+
+
+logging.basicConfig(filename='info.log',
+                            filemode='a',
+                            format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
+
+
+def send_challenge(challenge_to_solve):
+    try:
+        client = slack.WebClient(token='')
+        client.chat_postMessage(channel='coding_challenges', text=challenge_to_solve)
+    except Exception as error:
+        logging.error("--Impossible to communicate with slack--")
+        logging.exception(error)
+
+
+
+def fetch_random_challenge():
+
+    path_to_file = "challenges.txt"
+    file = open(path_to_file)
+    # Fetch all lines from the file
+    lines = open(path_to_file).readlines()
+    # calculate number of challenges available
+    number_of_challenges = len(lines)
+    logging.info("Almost got a challenge")
+    # Pick a random challenge from line [0](first line) to N-1
+    send_challenge(lines[random.randint(0,number_of_challenges-1)])
+    logging.info("Challenge sent successfully")
+
+
+
+if __name__ == '__main__':
+    fetch_random_challenge()
+
+

b350f75f7ec1625587d0eaa1981ca6006455e78c
Davide Pollicino <davidepollicino2015@gmail.com>
2021-11-30 18:41:37 +0000

    Revert "Make bot simple"
    This reverts commit 6805062fad507d3c4288551b327dd8e9a9c7c6b7.
    dsdsd

--- get_challenge.py
+++ get_challenge.py
@@ -1,15 +1,34 @@
 
 import slack
 import random 
+import logging
+import traceback
 import sys
+import mysql_connection
+
+
+# Configure Basic Logging
+logging.basicConfig(filename='log/error.log',
+                            filemode='a',
+                            format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.ERROR)
+
+
+logging.basicConfig(filename='log/info.log',
+                            filemode='a',
+                            format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
 
 
 def send_challenge(challenge_to_solve):
     try:
         client = slack.WebClient(token='')
         client.chat_postMessage(channel='coding_challenges', text=challenge_to_solve)
-    except:
-        print('error sending challenge via slack')
+    except Exception as error:
+        logging.error("--Impossible to communicate with slack--")
+        logging.exception(error)
 
 
 # Fetch random challenge from a file; I will use a database instead
@@ -20,15 +39,18 @@ def fetch_random_challenge():
     lines = open(path_to_file).readlines()
     # calculate number of challenges available
     number_of_challenges = len(lines)
+    logging.info("Almost got a challenge")
     # Pick a random challenge from line [0](first line) to N-1
-    return lines[random.randint(0,number_of_challenges-1)]
+    send_challenge(lines[random.randint(0,number_of_challenges-1)])
+    logging.info("Challenge sent")
+
 
 
 if __name__ == '__main__':
-    # connection = mysql_connection.connect()
-    # rand_challenge = mysql_connection.fetch_challenge_from_database(connection)
-    challenge = fetch_random_challenge()
-    # connection.close()
-    send_challenge(challenge)
+    connection = mysql_connection.connect()
+    rand_challenge = mysql_connection.fetch_challenge_from_database(connection)
+    # fetch_random_challenge()
+    connection.close()
+    send_challenge(rand_challenge)
 
 
